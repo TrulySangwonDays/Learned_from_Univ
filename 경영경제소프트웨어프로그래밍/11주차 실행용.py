efd81c7be21d3cca866dff07c99019e787b88e3e
TrulySangwonDays <01wkdwkd@naver.com>
2024-05-14 18:33:37 +0900

    feat : 11주차 실습파일 업로드

---
+++ 경영경제소프트웨어프로그래밍/11주차 실행용.py
@@ -0,0 +1,32 @@
+
+## 버튼 넣기
+
+from tkinter import *
+window = Tk()
+
+#=========================================================
+
+button1 = Button(window, text = '파이썬 종료', fg='red', command=exit)
+button1.pack()
+
+#=========================================================
+
+window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
